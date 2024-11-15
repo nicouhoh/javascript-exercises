dec0bc4a4d64a9d35b6c230e710edbb0d6b13501
nicouhoh <nicouhoh@gmail.com>
2024-11-14 22:53:23 -0800

    Completed exercises up through 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  result = [];
+  for (const element of array) {
+    if (!values.includes(element)) {
+      result.push(element);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (low, high) {
+  if (!Number.isInteger(low) || !Number.isInteger(high)) return "ERROR";
+  if (low < 0 || high < 0) return "ERROR";
+  let result = 0;
+  for (let i = Math.min(low, high); i <= Math.max(low, high); i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
