df55f96a678ead7c6c7270995a06040264b28f4e
nicouhoh <nicouhoh@gmail.com>
2024-11-15 19:17:45 -0800

    Complete problems 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  return parseFloat(((f - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  return parseFloat((c * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
